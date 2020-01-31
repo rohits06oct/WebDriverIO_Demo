733c8249980ca3c112fc4dfd1efdb9f104590d3c
rohits06oct <rohits.06oct@gmail.com>
2020-01-31 13:10:04 +0530

    Latest TopEvent

--- src/steps/Topevent.js
+++ src/steps/Topevent.js
@@ -1,25 +1,27 @@
 import { Given, When, Then } from 'cucumber';
 
-let url= "https://www.espn.com/";   //SEWEB-10789
+let url= "https://www.espn.in?_adblock=true&src=com&adtar=pc12345";   //SEWEB-10789
 let assert= require('chai').assert;
 
 Given(/^I am a user on espn.com$/, function () {
+    browser.setWindowSize(1680, 1050);
     browser.url(url);
 });
 
 When(/^I click Top Events button$/, function () {
-    var str=browser.$("div.dropdown-wrapper.league-nav-desktop.desktop-dropdown.hoverable > button").getText();
-    assert.equal("TOP EVENTS",str);
     browser.$("div.dropdown-wrapper.league-nav-desktop.desktop-dropdown.hoverable > button").click();
-    browser.pause(5000);
 });
 
 Then(/^the dropdown menu appears$/, function () {
-    
-    //assert.equal("https://the-internet.herokuapp.com/secure", url);
+    var menu=browser.$('div.dropdown-wrapper.league-nav-desktop.desktop-dropdown.hoverable > ul').isDisplayed();
+    assert.equal(menu, true);
 });
 
 Then(/^the dropdown menu scrollable$/, function () {
-    
-    //assert.equal("https://the-internet.herokuapp.com/secure", url);
+    const last=$('ul > li:nth-child(20) > a');
+    //last.scrollIntoView();
+    let firststr=last.getText();
+    let laststr=browser.$('ul > li:nth-child(25) > a').getText();
+    assert.notEqual(firststr,laststr);
+    browser.pause(5000);
 });
